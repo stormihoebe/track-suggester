ee97232983be3f445a5126b1cba430f3a20b61f1
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-02-17 15:37:07 -0800

    add simple true false quizzes based on answer, give result at bottom of page

--- js/scripts.js
+++ js/scripts.js
@@ -43,22 +43,96 @@ $(function(){
     var language = $("#language").val();
 
     $("#languageQuizzes div").hide();
-    
+
     if (language === "java") {
       $(".javaQuiz").show();
+    } else if (language === "css"){
+      $(".bad").show(); //I was having trouble with the naming the class of this, it wouldn't let me use logical names, but "bad" seemed to work...smh. I must have it defined elsewhere, I just can't find it.
+    } else if (language === "javaScript") {
+      $(".javaScriptQuiz").show();
+    } else if (language === "c"){
+      $(".cQuiz").show();
+    } else if (language === "php") {
+      $(".phpQuiz").show();
+    } else if (language === "drupal") {
+      $(".drupQuiz").show();
     }
-
   });
+
   $("#javaQuiz").submit(function(){
     event.preventDefault();
     var answer = $("#javaTF").val();
-    $("#languageQuizzes div").hide();
 
     if (answer){
       if (answer === "true") {
         $(".javaResults").text("You got: 0/1 correct. Try again");
       } else {
         $(".javaResults").text("You got: 1/1 correct. Great Job!");
+      };
+    };
+  });
+
+  $("#cssQuiz").submit(function(){
+    event.preventDefault();
+    var answer = $("#cssTF").val();
+
+    if (answer){
+      if (answer === "true") {
+        $(".cssResults").text("You got: 0/1 correct. Try again");
+      } else {
+        $(".cssResults").text("You got: 1/1 correct. Great Job!");
+      };
+    };
+  });
+
+  $("#jsQuiz").submit(function(){
+    event.preventDefault();
+    var answer = $("#jsTF").val();
+
+    if (answer){
+      if (answer === "false") {
+        $(".jsResults").text("You got: 0/1 correct. Try again");
+      } else {
+        $(".jsResults").text("You got: 1/1 correct. Great Job!");
+      };
+    };
+  });
+
+  $("#cSharpQuiz").submit(function(){
+    event.preventDefault();
+    var answer = $("#cTF").val();
+
+    if (answer){
+      if (answer === "false") {
+        $(".cResults").text("You got: 0/1 correct. Try again");
+      } else {
+        $(".cResults").text("You got: 1/1 correct. Great Job!");
+      };
+    };
+  });
+
+  $("#pQuiz").submit(function(){
+    event.preventDefault();
+    var answer = $("#phpTF").val();
+
+    if (answer){
+      if (answer === "false") {
+        $(".phpResults").text("You got: 0/1 correct. Try again");
+      } else {
+        $(".phpResults").text("You got: 1/1 correct. Great Job!");
+      };
+    };
+  });
+
+  $("#drupalQuiz").submit(function(){
+    event.preventDefault();
+    var answer = $("#drupalTF").val();
+
+    if (answer){
+      if (answer === "true") {
+        $(".drupalResults").text("You got: 0/1 correct. Try again");
+      } else {
+        $(".drupalResults").text("You got: 1/1 correct. Great Job!");
       };
     };
   });
