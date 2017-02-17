3e509a74feebd8985e2c7fe83b40096ee3484314
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-02-17 09:38:37 -0800

    set up variables in scripts file

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+  $("#epicodusForm").submit(function(){
+    event.preventDefault();
+      var firstName = $(".firstName").val();
+      var companyType = $("#companyType").val();
+      var system = $("input:radio[name=system]:checked").val();
+      var jobType = $("#jobType").val();
+      var experience = $("input:radio[name=experience]:checked").val();
+
+
+
+
+
+
+
+
+  });
+});
